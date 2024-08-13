23d43746f1bd62d6807dd874703f4c363c0d4ec2
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2024-08-13 21:16:43 +0200

    Included transition animation between cards

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -8,22 +8,44 @@ import IStep from '../../models/IStep';
   templateUrl: './escena.component.html',
   styleUrl: './escena.component.scss'
 })
+
 export class EscenaComponent {
 
   @Input()  arraySteps: IStep[] = [];
-
   
   currentStep = 0;
+  animating = false;
+  animationClass = '';
 
-  nextStep(){
-    if (this.currentStep < this.arraySteps.length -1){
-      this.currentStep++;
+
+
+  changeStep(targetStep: number) {
+    if (this.animating || targetStep === this.currentStep) return;
+
+    const direction = targetStep > this.currentStep ? 'next' : 'previous';
+    this.animating = true;
+    this.animationClass = direction === 'next' ? 'slide-out-left' : 'slide-out-right';
+
+    setTimeout(() => {
+      this.currentStep = targetStep;
+      this.animationClass = direction === 'next' ? 'slide-in-right' : 'slide-in-left';
+
+      setTimeout(() => {
+        this.animationClass = '';
+        this.animating = false;
+      }, 500);
+    }, 500);
+  }
+
+  nextStep() {
+    if (this.currentStep < this.arraySteps.length - 1) {
+      this.changeStep(this.currentStep + 1);
     }
   }
 
-  previousStep(){
-    if (this.currentStep >0){
-      this.currentStep--;
+  previousStep() {
+    if (this.currentStep > 0) {
+      this.changeStep(this.currentStep - 1);
     }
   }
   
